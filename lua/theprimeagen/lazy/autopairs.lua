8efd20c5ce29cd574dcb453a0158c446238eccd9
kanishkdhebana <dhebanakanishk@gmail.com>
2025-02-08 13:19:57 +0530

    added autopairs

---
+++ lua/theprimeagen/lazy/autopairs.lua
@@ -0,0 +1,15 @@
+return {
+  "windwp/nvim-autopairs",
+  event = "InsertEnter",
+  config = function()
+    require("nvim-autopairs").setup({
+      check_ts = true, -- Enable Treesitter integration for better behavior
+    })
+
+    -- Optional: Integrate with nvim-cmp for auto-completion support
+    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+    local cmp = require("cmp")
+    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+  end,
+}
+
